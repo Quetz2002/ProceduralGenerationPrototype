f6099dcb7baba9704001c5d1b32b31a52fd31f31
Quetz2002 <quetz.yggdrasil24@outlook.com>
2026-05-26 14:03:15 -0600

    Biomas

--- Assets/Scripts/ProceduralGeneration/ColourSettings.cs
+++ Assets/Scripts/ProceduralGeneration/ColourSettings.cs
@@ -1,8 +1,31 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 [CreateAssetMenu()]
 public class ColourSettings : ScriptableObject
 {
-    public Gradient gradient;
+    //public Gradient gradient;
     public Material planetMaterial;
+    public BiomeColourSettings biomeColourSettings;
+
+    [System.Serializable]
+    public class BiomeColourSettings
+    {
+        public Biome[] biomes;
+        public NoiseSettings noise;
+        public float noiseOffset;
+        public float noiseStrength;
+        [Range(0.0f, 1.0f)]
+        public float blendAmount;
+        [System.Serializable]
+        public class Biome
+        {
+            public Gradient gradient;
+            public Color tint;
+            [Range(0f, 1f)]
+            public float startHeight;
+            [Range (0f, 1f)]
+            public float tintPercent;
+        }
+    }
 }
